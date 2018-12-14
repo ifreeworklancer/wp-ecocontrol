3a234cbdb31db51e2a2a72914d351c5a70ddf17e
ifreeworklancer <ifreeworklancer@gmail.com>
2018-12-14 17:17:03 +0200

    edit_help__btn

--- wp-content/themes/ecocontrol/category-facts.php
+++ wp-content/themes/ecocontrol/category-facts.php
@@ -5,7 +5,7 @@ get_header('secondary');
 
 ?>
 
-     <!-- News -->
+    <!-- News -->
     <section id="archive-news">
         <div class="container">
             <div class="row">
@@ -18,47 +18,11 @@ get_header('secondary');
                         </div>
                         <div class="content-body">
                             <div class="news-tabs">
-                                <div class="news-tabs-header">
-                                    <ul class="news-tabs-header-list">
-                                        <li class="news-tabs-header-list__item active"><?= __('[:ru]Последние[:ua]Останні'); ?></li>
-                                        <li class="news-tabs-header-list__item"><?= __('[:ru]Прошедшие[:ua]Минулі'); ?></li>
-                                    </ul>
-                                </div>
                                 <div class="news-tabs-body">
                                     <div class="news-tabs-body-item">
                                         <?php
                                         $args = array(
                                             'category_name' => 'facts',
-                                            'posts_per_page' => 3,
-                                        );
-
-                                        $query = new WP_Query($args);
-
-                                        if ($query->have_posts()) {
-                                            while ($query->have_posts()) {
-                                                $query->the_post();
-                                                echo
-                                                    '<div class="news-tabs-body-item-row">'
-                                                    . '<div class="content order-2 order-lg-1">'
-                                                    . '<h5 class="title"><a href="' . get_post_permalink() . '">' . get_the_title() . '</a></h5>'
-                                                    . '<div class="info d-flex align-items-center">'
-                                                    . '<div class="info__data">' . get_field('news_data') . '</div>'
-                                                    . '<div class="info__author">' . get_field('news_author') . '</div>'
-                                                    . '</div>'
-                                                    . '<p class="text">' . get_the_excerpt() . '</p>'
-                                                    . '</div>'
-                                                    . '<div class="img order-1 order-lg-2" style="background-image: url(' . get_post_first_image_src() . ')"></div>'
-                                                    . '</div>';
-                                            }
-                                        }
-                                        wp_reset_postdata();
-                                        ?>
-                                    </div>
-                                    <div class="news-tabs-body-item">
-                                        <?php
-                                        $args = array(
-                                            'category_name' => 'facts',
-                                            'offset' => 3,
                                         );
 
                                         $query = new WP_Query($args);
@@ -103,7 +67,6 @@ get_header('secondary');
             </div>
         </div>
     </section>
-
 
 
 <?php
